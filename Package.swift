87d43a2fb86d782c542bb0ea7f2bec30f04f838c
TizianoCoroneo <tizianocoroneo@me.com>
2023-05-02 15:21:31 +0200

    fixed sovran version

--- Package.swift
+++ Package.swift
@@ -21,9 +21,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
-            name: "Sovran",
             url: "https://github.com/segmentio/Sovran-Swift.git",
-            from: "1.0.3"
+            .exactItem("1.0.3")
         )
     ],
     targets: [
@@ -31,7 +30,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Segment",
-            dependencies: ["Sovran"]),
+            dependencies: [
+                .product(name: "Sovran", package: "Sovran-Swift")
+            ]),
         .testTarget(
             name: "Segment-Tests",
             dependencies: ["Segment"]),
